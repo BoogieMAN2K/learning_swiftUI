3192dca6c626920e5b8177169135b26363ed6c52
BoogieMAN2K <valejandria@gmail.com>
2024-02-02 14:03:33 -0300

    Initial Commit

---
+++ Learning SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learning SwiftUI
+//
+//  Created by Victor Gil Alejandria on 02/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learning SwiftUI/Learning_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_SwiftUIApp.swift
+//  Learning SwiftUI
+//
+//  Created by Victor Gil Alejandria on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Learning_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
